5bc59d7c04e520b96afb2ae08da16feb487818f9
Fernando Yurisich <fyurisich@oohg.org>
2020-06-19 13:21:22 -0300

    Comments removed

--- test.bat
+++ test.bat
@@ -1,12 +1,4 @@
 @echo off
-
-rem
-rem $Id$
-rem
-rem This batch file builds all the executables needed to run QPM.
-rem See ./doc/howtosvn.txt
-rem
-
 echo If you see this message then "Extra Run Batch" feature is working!
 echo This test is going to pause.
 pause
